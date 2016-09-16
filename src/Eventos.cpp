db689f8c8b5006123443bf68f2a03935da15ff8b
cesarvargas95 <cesarva95@gmail.com>
2016-09-15 20:34:10 -0600

    prueba

--- src/Eventos.cpp
+++ src/Eventos.cpp
@@ -1,4 +1,5 @@
 #include "Eventos.h"
+#include "VarAleatorias.h"
 
 Eventos::Eventos()
 {
@@ -11,8 +12,14 @@ Eventos::~Eventos()
 }
 
 void Eventos::arriboPaqC1()
-{
-
+{   if(Comp1.contadorDePaq > 100){
+            Comp1.contadorDePaq =1;
+    }
+    colaPaq.push_back(Comp1.contadorDePaq);
+ if((Comp1.proc1 == false || Comp1.proc2 == false) && Comp1.ventanaPaq <4){
+        libServC1(true);
+        ++Comp1.contadorDePaq;
+    }
 }
 
 void Eventos::arriboMsjC1()
@@ -20,9 +27,14 @@ void Eventos::arriboMsjC1()
 
 }
 
-void Eventos::libServC1()
-{
+void Eventos::libServC1(bool tipo)
+{//false= mensaje  true = paquete
+    if(tipo==false){
 
+
+    }else{
+
+    }
 }
 
 void Eventos::llegaAck()
