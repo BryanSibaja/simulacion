823891eba823e09016e402d27a8f7e73f154e389
cesarvargas95 <cesarva95@gmail.com>
2016-09-24 19:38:26 -0600

    a

--- include/Eventos.h
+++ include/Eventos.h
@@ -3,7 +3,7 @@
 
 #include "VarAleatorias.h"
 #include <deque>
-
+#include <limits>
 using namespace std;
 enum EVENTOS{ARRIBOPAQC1, ARRIBOMSJC1, LIBSERV1C1, LIBSERV2C1, LLEGAACK, DEVOLMSJC1, LLEGAMSJC2,
              LIBSERVC2, LLEGAPAQC3, LIBSERVC3, TEMP};
@@ -34,18 +34,23 @@ class Eventos
             bool Ser1 = true;
             bool Ser2 = true;
             int conPaq = 0;
+            int conMsj = 0;
             int evnPaq = 0;
             bool msj = true;
             deque<int> colPaq;
             deque<int> colMsj;
         }Comp1;
 
-        struct varC2{
+         struct varC2{
+            deque<int> msj;
+            bool serv = true;
+        }Comp2;
+
+         struct varC3{
             deque<int> paq;
             int ack;
             bool serv = true;
         }Comp3;
-
 };
 
 #endif // EVENTOS_H
--- src/Eventos.cpp
+++ src/Eventos.cpp
@@ -26,7 +26,14 @@ void Eventos::arriboPaqC1()
 }
 
 void Eventos::arriboMsjC1()
+
 {
+    Comp1.colMsj.push_back(Comp1.conMsj);
+    ++Comp1.conMsj;
+    if(Comp1.Ser1)
+        tiempo[LIBSERV1C1] = *reloj;
+    if(Comp1.Ser2)
+        tiempo[LIBSERV2C1] = *reloj;
     tiempo[ARRIBOMSJC1] = *reloj + aleatorio.genNormal(4, 0.01);
 }
 
@@ -40,30 +47,44 @@ void Eventos::libServ1C1()
     else
     {
         if (!Comp1.colPaq.empty() && Comp1.evnPaq < 4)
-            cola = false; // se elige paq
-        else
+            cola = true ; // se elige paq
+        else if(!Comp1.colMsj.empty())
         {
-            if(!Comp1.colMsj.empty())
-                cola = true; // se elige msj
+            cola = false; // se elige msj
         }
     }
-    if (cola)
+
+    if ( !Comp1.colPaq.empty() || !Comp1.colMsj.empty())
     {
-        Comp1.evnPaq++;
-        if(aleatorio.porcentaje(95))
+        if (cola)
         {
-            float duracion = aleatorio.genExp(0.5);
-            tiempo[LLEGAPAQC3] = *reloj + duracion + 2;
-            tiempo[LIBSERV1C1] = *reloj + duracion;
+            Comp1.evnPaq++;
+            if(aleatorio.porcentaje(95))
+            {
+                float duracion = aleatorio.genExp(0.5);
+                tiempo[LLEGAPAQC3] = *reloj + duracion + 2;
+                tiempo[LIBSERV1C1] = *reloj + duracion;
+            }
         }
-    }else{
+        else
+        {
+            // mensajes
+            tiempo[LIBSERV1C1] = *reloj + 1;
+            tiempo[LLEGAMSJC2]= *reloj+4; // 3 del tiempo de propagacion y 1 de distribución de probabilidad cuya
+                                            // función de densidad es: f(x) = 2x/3 con x entre 1 y 2 segundos.
+        }
+    }
+    else
+    {
+        tiempo[LIBSERV1C1] = *reloj + 2;
     }
 }
 
 
 void Eventos::llegaAck()
 {
-    if( Comp3.ack == Comp1.colPaq[Comp1.evnPaq]){
+    if( Comp3.ack == Comp1.colPaq[Comp1.evnPaq])
+    {
         for(int i = 0; i < Comp1.evnPaq; i++)
             Comp1.colPaq.pop_front();
     }
@@ -72,36 +93,72 @@ void Eventos::llegaAck()
 
 void Eventos::devolMsjC1()
 {
+    Comp1.colMsj.push_back(Comp2.msj.front());
+    Comp2.msj.pop_front();
+    tiempo[DEVOLMSJC1] = numeric_limits<float>::infinity();
 
 }
 
 void Eventos::llegaMsjC2()
 {
-
+    Comp2.msj.push_back(Comp1.colMsj.front());
+    Comp1.colMsj.pop_front();
+    if(!Comp2.msj.empty() && Comp3.serv )
+    {
+        tiempo[LIBSERVC2] = *reloj;
+    }
+    tiempo[LLEGAMSJC2]= numeric_limits<float>::infinity();
 }
 
 void Eventos::libServC2()
 {
+    if (!Comp2.msj.empty())
+    {
+        if(aleatorio.porcentaje(80))
+        {
+            Comp2.msj.pop_front();
+            tiempo[LIBSERVC2] = *reloj+aleatorio.genExp(1);
+        }
+        else
+        {
+           float duracion = aleatorio.genExp(1);
+           tiempo[DEVOLMSJC1] = *reloj+ duracion+3;
+            tiempo[LIBSERVC2] = *reloj+duracion;
 
+        }
+
+    }
+    else
+    {
+        tiempo[LIBSERVC2]= numeric_limits<float>::infinity();
+    }
 }
 
 void Eventos::llegaPaqC3()
 {
     Comp3.paq.push_back(Comp1.colPaq[Comp1.evnPaq]);
     if(Comp3.serv)
+    {
         tiempo[LIBSERVC3] = *reloj;
+    }
+    tiempo[LLEGAPAQC3]= numeric_limits<float>::infinity();
 }
 
 void Eventos::libServC3()
 {
-    if(Comp3.ack == Comp3.paq.front()){
+    if(Comp3.ack == Comp3.paq.front())
+    {
         Comp3.ack++;
         Comp3.paq.pop_front();
+        tiempo[LIBSERVC3]= *reloj+ aleatorio.genNormal(1.5, 0.01);
     }
-    else{
-        while(!Comp3.paq.empty()){
+    else
+    {
+        while(!Comp3.paq.empty())
+        {
             Comp3.paq.pop_front();
         }
+        tiempo[LIBSERVC3] = numeric_limits<float>::infinity();
     }
     tiempo[LLEGAACK] = *reloj + 2;
 }
